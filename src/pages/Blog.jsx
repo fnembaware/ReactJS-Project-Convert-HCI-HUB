90fb4adb4608efbd830decffaa8ac9e10afb5052
fnembaware <fadzai@tlu.ee>
2024-05-31 23:43:59 +0300

    Adding images to the public folder

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,6 +1,40 @@
-import React from 'react'
+import React,{useEffect} from 'react'
+import { Link } from 'react-router-dom'
 
 const Blog = () => {
+
+  useEffect(() => {
+        document.title = "Blog - Tallinn University HCI Hub";
+      }, []);
+
+  const data = [
+    {
+      img: './Blog images/katja-anokhina-_7ceGXTAtyQ-unsplash.jpg',
+      heading: 'Learn about HCI in the Easiest Way',
+      description: `Discover the fascinating world of Human-Computer Interaction effortlessly with our user-friendly resources and engaging content. Whether you're a novice or an experienced professional, dive into the world of HCI in the easiest way possible, unlocking insights that make learning both accessible and enjoyable`,
+      blogLink: 'blog1'
+    },
+    {
+      img: './images/airfocus-v89zhr0iBFY-unsplash.jpg',
+      heading: 'Design Thinking and Gererative Research Workshop',
+      description: `Design thinking is a human-centered problem-solving approach that emphasizes empathy, ideation, and prototyping to create innovative solutions. In a generative research workshop, participants engage in collaborative activities to explore and generate insights, fostering a creative environment that fuels the ideation phase of design processes.`,
+      blogLink: 'blog2'
+    },
+    {
+      img: './images/brooke-cagle--uHVRvDr7pg-unsplash.jpg',
+      heading: 'Collaboration Sessions',
+      description: `Collaboration in Human-Computer Interaction (HCI) involves interdisciplinary teamwork, bringing together experts from diverse fields such as design, psychology, and technology to create user-centered solutions. This collaborative approach ensures a holistic understanding of user needs and perspectives, fostering the development of more effective and inclusive interactive systems.`,
+      blogLink: 'blog3'
+    },
+  ];
+
+  const categories = ['Technology Trends',
+    'User Experience (UX)',
+    'Industry Insights',
+    'Expert Interviews',
+    'Case Studies',
+    'Tool Reviews and Tutorials'
+  ];
   return (
     <>
       <section>
@@ -8,50 +42,33 @@ const Blog = () => {
       </section>
 
       <section id="blog-container">
-        <div class="blogs">
-          <div class="post">
-            {/* <img src="Blog images/katja-anokhina-_7ceGXTAtyQ-unsplash.jpg" alt="https://unsplash.com/photos/a-person-holding-a-robotic-hand-in-front-of-a-mirror-_7ceGXTAtyQ"> */}
-            <h3>Learn about HCI in the Easiest Way</h3>
-            <p>Discover the fascinating world of Human-Computer Interaction effortlessly with our user-friendly resources and engaging content. Whether you're a novice or an experienced professional, dive into the world of HCI in the easiest way possible, unlocking insights that make learning both accessible and enjoyable</p>
-            <a href="blog1.html">Read more</a>
-          </div>
+        <div className="blogs">
 
-          <div class="post">
-            {/* <img src="images/airfocus-v89zhr0iBFY-unsplash.jpg" > */}
-            <h3>Design Thinking and Gererative Research Workshop</h3>
-            <p>Design thinking is a human-centered problem-solving approach that emphasizes empathy, ideation, and prototyping to create innovative solutions. In a generative research workshop, participants engage in collaborative activities to explore and generate insights, fostering a creative environment that fuels the ideation phase of design processes.</p>
-            <a href="blog2.html">Read more</a>
-          </div>
+          {
+            data.map((item, index) => (
+              <div className="post" key={index}>
+                <img src={item.img} alt={index} />
+                <h3>{item.heading}</h3>
+                <p>{item.description}</p>
+                <Link to={'/' + item.blogLink.toLocaleLowerCase()} target='_black' >Read more</Link>
+              </div>
+            ))
+          }
 
-          <div class="post">
-            {/* <img src="images/brooke-cagle--uHVRvDr7pg-unsplash.jpg"> */}
-            <h3>Collaboration Sessions</h3>
-            <p>Collaboration in Human-Computer Interaction (HCI) involves interdisciplinary teamwork, bringing together experts from diverse fields such as design, psychology, and technology to create user-centered solutions. This collaborative approach ensures a holistic understanding of user needs and perspectives, fostering the development of more effective and inclusive interactive systems.</p>
-            <a href="blog3.html">Read more</a>
-          </div>
         </div>
 
-        {/* <div class="cate">
+        <div className="cate">
           <h2>Categories</h2>
-          <hr>
-            <a href="#">Technology Trends</a>
-            <hr>
-              <hr>
-                <a href="#">User Experience (UX)</a>
-                <hr>
-                  <hr>
-                    <a href="#">Industry Insights</a>
-                    <hr>
-                      <hr>
-                        <a href="#">Expert Interviews</a>
-                        <hr>
-                          <hr>
-                            <a href="#">Case Studies</a>
-                            <hr>
-                              <hr>
-                                <a href="#">Tool Reviews and Tutorials</a>
-                                <hr>
-                                </div> */}
+          <hr />
+          <hr />
+          {categories.map((Categorie, i) => (
+            <div key={i}>
+              <a href="#"  >{Categorie}</a>
+              <hr />
+              <hr />
+            </div>
+          ))}
+        </div>
       </section>
     </>
   )
